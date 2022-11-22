3f8c4829caf1344e288f179c093aa77277dcd3c5
jacktho <jack@jacktho.com>
2022-11-22 14:13:17 -0500

    changed colorscheme
    changed colorscheme

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -1,6 +1,3 @@
 "require('lualine') Plugin lua settings
 lua require('jack')
 
-" Load the colorscheme
-colorscheme github_dark_default
-
--- nvim/.config/nvim/lua/jack/plugin-settings/lualine.lua
+++ nvim/.config/nvim/lua/jack/plugin-settings/lualine.lua
@@ -3,7 +3,7 @@ local lualine = require('lualine')
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'codedark'
+    theme = 'tokyonight',
   },
   sections = {
     lualine_c = {
