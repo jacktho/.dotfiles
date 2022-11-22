04c848e8e2b1c0cd3601aae6b59160d25490d841
jacktho <jack@jacktho.com>
2022-11-22 14:22:26 -0500

    adding autopairs and better treesitter

--- nvim/.config/nvim/lua/jack/plugin-settings/lsp/mason.lua
+++ nvim/.config/nvim/lua/jack/plugin-settings/lsp/mason.lua
@@ -1,27 +1,27 @@
-local mason = require('mason')
-local mason_lspconfig = require('mason-lspconfig')
-local mason_null_ls = require('mason-null-ls')
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+local mason_null_ls = require("mason-null-ls")
 
 mason.setup()
 
 mason_lspconfig.setup({
-  ensure_installed = {
-    'dockerls',
-    'html',
-    'jsonls',
-    'sumneko_lua',
-    'yamlls',
-    'tsserver',
-    'tailwindcss',
-    'svelte',
-    'emmet_ls',
-  },
+	ensure_installed = {
+		"dockerls",
+		"html",
+		"jsonls",
+		"sumneko_lua",
+		"yamlls",
+		"tsserver",
+		"tailwindcss",
+		"svelte",
+		"emmet_ls",
+	},
 })
 
 mason_null_ls.setup({
-  ensure_installed = {
-    'prettier',
-    'stylua',
-    'eslint_d',
-  }
- })
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"eslint_d",
+	},
+})
--- nvim/.config/nvim/lua/jack/plugin-settings/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/jack/plugin-settings/nvim-treesitter.lua
@@ -1,17 +1,33 @@
-require'nvim-treesitter.configs'.setup {
-  -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = {"svelte", "javascript", "comment", "css", "graphql", "html", "json", "lua", "make", "typescript", "yaml", "vim" },
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
+local treesitter = require("nvim-treesitter.configs")
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = true,
-  },
-  indent = {
-    enable = true
-  }
-}
+treesitter.setup({
+	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {
+		"svelte",
+		"javascript",
+		"comment",
+		"dockerfile",
+		"css",
+		"graphql",
+		"html",
+		"json",
+		"bash",
+		"markdown",
+		"tsx",
+		"lua",
+		"make",
+		"typescript",
+		"yaml",
+		"vim",
+	},
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+	},
+})
