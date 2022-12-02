fdb8d57a12f423039f120f3f112f94fff82fe25b
jacktho <jack@jacktho.com>
2022-12-02 13:24:52 -0500

    h and l can now be used to navigate directories

--- nvim/.config/nvim/lua/jack/plugin-settings/nvim-tree.lua
+++ nvim/.config/nvim/lua/jack/plugin-settings/nvim-tree.lua
@@ -1,26 +1,35 @@
-local nvimTree = require('nvim-tree')
+local nvimTree = require("nvim-tree")
 
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#E97207 ]])
 
+local customMappings = {
+	{ key = "h", action = "close_node" },
+	{ key = "l", action = "edit" },
+}
+
 nvimTree.setup({
-  view = {
-    adaptive_size = true,
-  },
-  renderer = {  
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = '',
-          arrow_open = '',
-        }
-      }
-    }
-  },
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false,
-      }
-    }
-  } 
+	view = {
+		adaptive_size = true,
+		mappings = {
+			custom_only = false,
+			list = customMappings,
+		},
+	},
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "",
+					arrow_open = "",
+				},
+			},
+		},
+	},
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
 })
