7b2c58226125309660f6f33a589c7f15b02f5c78
jacktho <jack@jacktho.com>
2022-12-05 17:39:56 -0500

    formatting?

--- nvim/.config/nvim/lua/jack/plugin-settings/telescope.lua
+++ nvim/.config/nvim/lua/jack/plugin-settings/telescope.lua
@@ -1,15 +1,16 @@
-local telescope = require('telescope')
-local actions = require('telescope.actions')
+local telescope = require("telescope")
+local actions = require("telescope.actions")
 
 telescope.setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-      },
-    },
-  },
+	defaults = {
+		layout_strategy = "center",
+		mappings = {
+			i = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+			},
+		},
+	},
 })
 
-telescope.load_extension('fzf')
+telescope.load_extension("fzf")
