e2d904ff4e825cad2c5970b2f4cb95d7878d4bde
jacktho <jack@jacktho.com>
2023-11-17 17:26:23 -0500

    formatting

--- nvim/.config/nvim/lua/jack/options.lua
+++ nvim/.config/nvim/lua/jack/options.lua
@@ -1,5 +1,14 @@
 local opt = vim.opt
 
+-- neovide options
+if vim.g.neovide then
+	vim.g.neovide_transparency = 0.7
+	vim.g.neovide_hide_mouse_when_typing = true
+	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+	vim.g.neovide_cursor_vfx_particle_lifetime = 3
+	vim.g.neovide_cursor_vfx_particle_density = 20.0
+end
+
 -- Disabled for nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -12,7 +21,8 @@ opt.clipboard:append("unnamedplus")
 
 -- Mouse
 opt.mouse = "a"
-opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+opt.guicursor =
+	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 -- Line numbers
 opt.number = true
@@ -29,7 +39,7 @@ opt.wrap = false
 
 -- Undo
 opt.undofile = true
-opt.undodir="/home/jack/.config/nvim/undo"
+opt.undodir = "/home/jack/.config/nvim/undo"
 
 -- Cursor line
 opt.cursorline = true
